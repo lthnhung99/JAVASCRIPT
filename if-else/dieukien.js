e5a988715511e538860820810c35f6d6a73064bd
lthn99 <lenhung14101999@gmail.com>
2023-03-14 21:15:58 +0700

    create ex

---
+++ if-else/dieukien.js
@@ -0,0 +1,29 @@
+let dtb = 15;
+    if(dtb >= 8 && dtb < 10){
+        console.log("Giỏi");
+    }
+    if(dtb >= 5 && dtb < 8){
+        console.log("Khá");
+    }
+    if(dtb >=0 && dtb <5){
+        console.log("Yếu");
+    }
+    // false && true => false
+    if(dtb <0 || dtb >10){
+        console.log("Diem khong hop le");
+    }
+
+    let dtb = 15;
+    if (dtb >= 9 && dtb < 10) {
+        console.log("Xuất sắc");
+    } else if (dtb >= 8 && dtb < 9) {
+        console.log("Giỏi");
+    } else if (dtb >= 7 && dtb < 8) {
+        console.log("Khá");
+    } else if (dtb >= 5 && dtb < 7) {
+        console.log("Trung bình");
+    } else if (dtb >= 0 && dtb < 5) {
+        console.log("Yếu");
+    } else {
+        console.log("Điểm không hợp lệ");
+    }
